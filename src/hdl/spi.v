382046bf5f53b54869d49a8543f0f1bde664bcc7
Johannes Pfau <johannes.pfau@kit.edu>
2025-01-16 10:34:50 +0000

    Ultra simple SPI module

---
+++ src/hdl/spi.v
@@ -0,0 +1,36 @@
+`timescale 1ns/1ps
+
+module spi (
+    input clk,
+    input mosi,
+    input nss,
+
+    output[7:0] adsr_ai, adsr_di, adsr_s, adsr_ri,
+    output[31:0] osc_count,
+    output[15:0] filter_a, filter_b,
+    output mute,
+    output trig
+);
+
+    // Mute during programming
+    assign mute = ~nss;
+    // FIXME: Should support trigger using SPI, but this should not mute...
+    assign trig = 1'b0;
+    
+    reg[95:0] cfg;
+    always @(posedge clk) begin
+        if (nss == 1'b0) begin
+            cfg <= {cfg[94:0], mosi};
+        end
+    end
+
+    assign adsr_ai[7:0] = cfg[7:0];
+    assign adsr_di[7:0] = cfg[15:8];
+    assign adsr_s[7:0] = cfg[23:16];
+    assign adsr_ri[7:0] = cfg[31:24];
+
+    assign osc_count[31:0] = cfg[63:32];
+    assign filter_a[15:0] = cfg[79:64];
+    assign filter_b[15:0] = cfg[95:80];
+
+endmodule
---
+++ src/sim/spi_tb.v
@@ -0,0 +1,49 @@
+`timescale 1ns/1ps
+
+module spi_tb();
+
+    reg clk, nss, mosi;
+
+    initial begin
+        $dumpfile("out/sim/spi_tb.vcd");
+        $dumpvars();
+
+        clk = 0;
+        #5000 $finish;
+    end
+    always #5 clk = ~clk;
+
+    reg [95:0] data_in;
+    integer i;
+    initial begin
+        data_in = 96'h123456789ABCDEF012345678;
+
+        nss = 1'b1;
+        #50 nss = 1'b0;
+        #5;
+        for (i = 0; i < 96; i = i + 1) begin
+            #10 data_in = data_in << 1;
+        end
+        nss = 1'b1;
+    end
+    assign mosi = data_in[95];
+
+
+    wire[7:0] adsr_ai, adsr_di, adsr_s, adsr_ri;
+    wire[31:0] osc_count;
+    wire[15:0] filter_a, filter_b;
+    wire mute;
+    wire trig;
+    spi uut (
+        .clk(clk),
+        .mosi(mosi),
+        .nss(nss),
+
+        .adsr_ai(adsr_ai), .adsr_di(adsr_di), .adsr_s(adsr_s), .adsr_ri(adsr_ri),
+        .osc_count(osc_count),
+        .filter_a(filter_a), .filter_b(filter_b),
+        .mute(mute),
+        .trig(trig)
+    );
+
+endmodule
