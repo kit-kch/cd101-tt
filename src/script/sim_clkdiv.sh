487c5e695446ce19740f3efccb12aee840e124cf
Johannes Pfau <johannes.pfau@kit.edu>
2025-01-23 13:13:51 +0000

    Simulation scripts: Create folder if does not exist

--- src/script/sim_clkdiv.sh
+++ src/script/sim_clkdiv.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
 
+mkdir -p build/sim
 verilator -cc src/sim/clkdiv_tb.v src/hdl/clkdiv.v src/hdl/dff.v -Mdir build/verilator --binary --trace
 ./build/verilator/Vclkdiv_tb
--- src/script/sim_filter.sh
+++ src/script/sim_filter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
 
+mkdir -p build/sim
 verilator -cc src/sim/filter_tb.v src/hdl/filter.v -Mdir build/verilator --binary --trace
 ./build/verilator/Vfilter_tb
